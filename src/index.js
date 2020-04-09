57087372c0ebea6c9cce68b9e7d9d47a2fe370d3
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-09 16:32:41 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function candyReducer(state = [], action) {
 
 function render() {
   let container = document.getElementById('container');
-  if(store.getState()) {
+  if (store.getState()) {
     container.textContent = store.getState().join(' ')
   } else {
     throw new Error("the store's state has not been defined yet")
@@ -20,3 +20,23 @@ function render() {
 
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+}
+
+let store = createStore(candyReducer);
+store.dispatch({ type: '@@INIT' });
